40db7ddcd825c98cbe85b582e8b08bf0e78f5298
Cuong Hong Phu <55767773+HongPhuMagic@users.noreply.github.com>
2020-10-28 12:36:42 -0400

    first commit

---
+++ terra1.tf
@@ -0,0 +1,169 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+    }
+  }
+}
+
+
+provider "aws" {
+  region = "ca-central-1"
+  access_key = ""
+  secret_key = ""
+}
+
+
+# 1. Create vpc
+resource "aws_vpc" "vpc1" {
+  cidr_block = "10.0.0.0/16"
+}
+
+
+# 2. Create Internet Gateway
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.vpc1.id
+}
+
+
+# 3. Create Custom Route Table
+resource "aws_route_table" "route-table1" {
+  vpc_id = aws_vpc.vpc1.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  route {
+    ipv6_cidr_block        = "::/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "prod"
+  }
+}
+
+
+# 4. Create a Subnet
+resource "aws_subnet" "subnet-1" {
+    vpc_id = aws_vpc.vpc1.id
+    cidr_block = "10.0.1.0/24"
+    availability_zone = "ca-central-1a"
+
+    tags = {
+        Name = "cloud-ready"
+    }
+}
+
+
+# 5. Associate subnet with Route Table
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.subnet-1.id
+  route_table_id = aws_route_table.route-table1.id
+}
+
+
+# 6. Create security group to allow port 22, 80, 443 (ports we need for webtraffic)
+resource "aws_security_group" "allow_web" {
+  name        = "allow_web_traffic"
+  description = "Allow web inbound traffic"
+  vpc_id      = aws_vpc.vpc1.id
+
+  ingress {
+    description = "HTTPS"
+    from_port   = 443                            # range of ports 
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]                  
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80                          
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]                  
+  }
+
+  ingress {
+    description = "SSH"
+    from_port   = 22                          
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]                  
+  }
+  
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_web"
+  }
+}
+
+
+# 7. Create a network interface with an ip in the subnet that was created in step 4
+resource "aws_network_interface" "web-server-aar" {
+  subnet_id       = aws_subnet.subnet-1.id
+  private_ips     = ["10.0.1.50"]                       # give ip to the webserver
+  security_groups = [aws_security_group.allow_web.id]
+
+}
+
+
+# 8. Assign an elastic IP to the network interface created in step 7. Elastic ip allows anyone on the internet to connect to
+resource "aws_eip" "one" {
+  vpc                       = true
+  network_interface         = aws_network_interface.web-server-aar .id
+  associate_with_private_ip = "10.0.1.50"
+  depends_on                = [aws_internet_gateway.gw]
+}
+
+output "server_public_ip" {
+  value = aws_eip.one.public_ip
+}
+
+
+# 9. Create Ubuntu server and install/enable apache2
+resource "aws_instance" "first-ec2" { 
+  ami = "ami-0c2f25c1f66a1ff4d"
+  instance_type = "t2.micro"
+  availability_zone = "ca-central-1a"
+  key_name          = "EC2Tutorial"
+
+  network_interface {
+    device_index         = 0
+    network_interface_id = aws_network_interface.web-server-aar.id
+  }
+
+  user_data = <<-EOF
+                #!/bin/bash
+                sudo yum update -y
+                sudo yum install apache2 -y
+                sudo systemctl start apache2
+                sudo bash -c 'echo your very first web server > /var/www/html/index.html'
+
+                sudo yum install docker -y 
+
+                EOF
+  tags = {
+    Name = "web-server"
+  }
+}
+
+
+
+# output "server_private_ip" {
+#   value = aws_instance.first-ec2.private_ip
+
+# }
+
+# output "server_id" {
+#   value = aws_instance.first-ec2.id
+# }
