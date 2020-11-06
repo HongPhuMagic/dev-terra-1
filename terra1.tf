d3b524b2c34b80eb225ea6c5a99bfba201871201
Cuong Hong Phu <55767773+HongPhuMagic@users.noreply.github.com>
2020-11-06 11:34:28 -0500

    updates to tf

--- terra1.tf
+++ terra1.tf
@@ -9,14 +9,15 @@ terraform {
 
 provider "aws" {
   region = "ca-central-1"
-  access_key = ""
-  secret_key = ""
+  access_key = "AKIA2JPSVGVWWI4IE6HU"  # var.cred_ac
+  secret_key = "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"  # var.cred_sc
 }
 
 
 # 1. Create vpc
 resource "aws_vpc" "vpc1" {
   cidr_block = "10.0.0.0/16"
+  enable_dns_hostnames = true
 }
 
 
@@ -109,7 +110,7 @@ resource "aws_security_group" "allow_web" {
 
 
 # 7. Create a network interface with an ip in the subnet that was created in step 4
-resource "aws_network_interface" "web-server-aar" {
+resource "aws_network_interface" "web-server-sol" {
   subnet_id       = aws_subnet.subnet-1.id
   private_ips     = ["10.0.1.50"]                       # give ip to the webserver
   security_groups = [aws_security_group.allow_web.id]
@@ -120,7 +121,7 @@ resource "aws_network_interface" "web-server-aar" {
 # 8. Assign an elastic IP to the network interface created in step 7. Elastic ip allows anyone on the internet to connect to
 resource "aws_eip" "one" {
   vpc                       = true
-  network_interface         = aws_network_interface.web-server-aar .id
+  network_interface         = aws_network_interface.web-server-sol.id
   associate_with_private_ip = "10.0.1.50"
   depends_on                = [aws_internet_gateway.gw]
 }
@@ -131,39 +132,64 @@ output "server_public_ip" {
 
 
 # 9. Create Ubuntu server and install/enable apache2
-resource "aws_instance" "first-ec2" { 
+resource "aws_instance" "second-ec2" { 
   ami = "ami-0c2f25c1f66a1ff4d"
   instance_type = "t2.micro"
   availability_zone = "ca-central-1a"
-  key_name          = "EC2Tutorial"
+  key_name          = "EC2 Tutorial"
 
   network_interface {
     device_index         = 0
-    network_interface_id = aws_network_interface.web-server-aar.id
+    network_interface_id = aws_network_interface.web-server-sol.id
+  }
+  
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum update -y"
+      "sudo yum install apache2 -y"
+      "sudo yum install git"
+      "sudo mkdir /home/ec2-user/automate-prometheus-ansible"
+      # "sudo mkdir /home/ec2-user/webapp"
+      "sudo cd /home/ec2-user/automate-prometheus-ansible"
+      "sudo git clone https://github.com/HongPhuMagic/automate-prometheus-ansible.git"
+      # "sudo ansible -m role_node_setup.yml"
+    ]
   }
 
-  user_data = <<-EOF
-                #!/bin/bash
-                sudo yum update -y
-                sudo yum install apache2 -y
-                sudo systemctl start apache2
-                sudo bash -c 'echo your very first web server > /var/www/html/index.html'
-
-                sudo yum install docker -y 
-
-                EOF
   tags = {
-    Name = "web-server"
+    Name = "league_web"
+  }
+
+  connection {
+    type = "ssh"
+    user = "ec2-user"
+    password = ""
+    private_key = "home/nonr/AWS/cloud.pem"
   }
 }
 
 
+output "server_private_ip" {
+  value = aws_instance.second-ec2.private_ip
+}
 
-# output "server_private_ip" {
-#   value = aws_instance.first-ec2.private_ip
+output "server_id" {
+  value = aws_instance.second-ec2.id
+}
 
-# }
 
-# output "server_id" {
-#   value = aws_instance.first-ec2.id
-# }
+
+
+
+
+#user_data = <<-EOF
+  #               #!/bin/bash
+  #               sudo yum update -y
+  #               sudo yum install apache2 -y
+                
+  #               sudo yum install docker -y 
+  #               sudo service docker start
+  #               sudo chmod 666 /var/run/docker.sock
+                
+  #               EOF
+# sudo docker run -p 80:5000 bankai9415/lol-web-headless
